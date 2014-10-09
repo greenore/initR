572e6f2fa8f5238b4cff1f6bd9f0b69752ca43cf
Tim Hagmann <greenore@users.noreply.github.com>
2014-10-09 15:28:37 +0200

    Update init.R

--- init.R
+++ init.R
@@ -10,82 +10,79 @@
 ################################################################################
 
 ## Helper Functions
-local({
+# Function to load libraries
+loadLibraries <- function(x){
+  for(i in seq_along(x)){
+    library(x[i], character.only=TRUE)
+  }
+}
 
-  # Function to load libraries
-  loadLibraries <- function(x){
-    for(i in seq_along(x)){
-      library(x[i], character.only=TRUE)
-    }
+# Function to install and/or load packages from CRAN 
+packagesCRAN <- function(requiredPackages, update=F){
+  missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
+  
+  if(length(missingPackages) > 0 || update){
+    if(update){missingPackages <- requiredPackages} # Base (required)
+    install.packages(missingPackages)
   }
   
-  # Function to install and/or load packages from CRAN 
-  packagesCRAN <- function(requiredPackages, update=F){
-    missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
-    
-    if(length(missingPackages) > 0 || update){
-      if(update){missingPackages <- requiredPackages} # Base (required)
-      install.packages(missingPackages)
-    }
-    
-    loadLibraries(requiredPackages)
+  loadLibraries(requiredPackages)
+}
+
+# Function to install and/or load missing packages from Bioconductor 
+packagesBioconductor <- function(requiredPackages, update=F){
+  missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
+  
+  if(length(missingPackages) > 0 || update){
+    if(update){missingPackages <- requiredPackages} # Base (required)
+    source("https://rawgit.com/greenore/initR/master/biocLite.R")
+    biocLite(missingPackages)
   }
   
-  # Function to install and/or load missing packages from Bioconductor 
-  packagesBioconductor <- function(requiredPackages, update=F){
-    missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
+  loadLibraries(requiredPackages)
+}
+
+# Function to install and/or load missing packages from Github 
+packagesGithub <- function(requiredPackages, repoName, authToken=NULL, 
+                           proxyUrl=NULL, port=NULL,
+                           update=F){
+  packagesCRAN('devtools')
+  
+  missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
+  
+  if(length(missingPackages) > 0 || update){
+    setProxy(proxyUrl=proxyUrl, port=port)
+    missingPackages <- paste0(repoName, '/', missingPackages)    # Base (missing)
     
-    if(length(missingPackages) > 0 || update){
-      if(update){missingPackages <- requiredPackages} # Base (required)
-      source("https://rawgit.com/greenore/initR/master/biocLite.R")
-      biocLite(missingPackages)
+    if(update) {
+      missingPackages <- paste0(repoName, '/', requiredPackages) # Base (required)
     }
     
-    loadLibraries(requiredPackages)
+    for(i in 1:length(missingPackages)){
+      install_github(repo=missingPackages[i], auth_token=authToken)
+    }    
   }
   
-  # Function to install and/or load missing packages from Github 
-  packagesGithub <- function(requiredPackages, repoName, authToken=NULL, 
-                             proxyUrl=NULL, port=NULL,
-                             update=F){
-    packagesCRAN('devtools')
-    
-    missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
-    
-    if(length(missingPackages) > 0 || update){
-      setProxy(proxyUrl=proxyUrl, port=port)
-      missingPackages <- paste0(repoName, '/', missingPackages)    # Base (missing)
-      
-      if(update) {
-        missingPackages <- paste0(repoName, '/', requiredPackages) # Base (required)
-      }
-      
-      for(i in 1:length(missingPackages)){
-        install_github(repo=missingPackages[i], auth_token=authToken)
-      }    
-    }
-    
-    loadLibraries(requiredPackages)
-  }
+  loadLibraries(requiredPackages)
+}
+
+# Function to ping a server (i.e., does the server exist)
+pingServer <- function(url, stderr=F, stdout=F, ...){
+  vec <- suppressWarnings(system2("ping", url, stderr=stderr, stdout=stdout, ...))
+  if (vec == 0){TRUE} else {FALSE}
+}
+
+setProxy <- function(proxyUrl, port){
+  packagesCRAN(httr)
   
-  # Function to ping a server (i.e., does the server exist)
-  pingServer <- function(url, stderr=F, stdout=F, ...){
-    vec <- suppressWarnings(system2("ping", url, stderr=stderr, stdout=stdout, ...))
-    if (vec == 0){TRUE} else {FALSE}
-  }
+  port <- as.numeric(port)
   
-  setProxy <- function(proxyUrl, port){
-    packagesCRAN(httr)
+  if(pingServer(proxyUrl)){
+    usr <- readline("Bitte Benutzername eingeben: ")
+    pwd <- readline("Bitte Passwort eingeben: ")
+    cat("\14")
     
-    port <- as.numeric(port)
-    
-    if(pingServer(proxyUrl)){
-      usr <- readline("Bitte Benutzername eingeben: ")
-      pwd <- readline("Bitte Passwort eingeben: ")
-      cat("\14")
-      
-      reset_config()
-      set_config(use_proxy(url=proxyUrl, port=port, username=usr, password=pwd))
-    }
+    reset_config()
+    set_config(use_proxy(url=proxyUrl, port=port, username=usr, password=pwd))
   }
-})
+}
