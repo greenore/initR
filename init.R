7d116c8ac1f2f14d38389f610fc09bdf08c670c4
Tim Hagmann <greenore@users.noreply.github.com>
2014-10-17 02:59:15 +0200

    change notation

--- init.R
+++ init.R
@@ -2,105 +2,111 @@
 # Purpose:         Small initialization script
 # Date:            2014-10-09
 # Author:          Tim Hagmann
-# Data Used:       
-# Packages Used:   devtools, httr
-# Machine:         X10004328 (Win 32)
-# Notes:
+# Notes:           WINDOWS: In order for it to work, RTools() has to be installed
 # R Version:       R version 3.1.1 -- "Sock it to Me"
 ################################################################################
 
-## Helper Functions
+## Library Functions
 # Function to load libraries
-loadLibraries <- function(requiredPackages){
-  requiredPackages <- gsub("@[[:alnum:]]{1, }", "", requiredPackages) # Remove @ dev etc.
-
-  for(i in seq_along(requiredPackages)){
-    library(requiredPackages[i], character.only=TRUE)
+loadLibraries <- function(required_packages){
+  required_packages_cut <- cutTxt(x=required_packages, identifier="@", cut2="right") # Remove @ dev etc.
+  
+  for(i in seq_along(required_packages_cut)){
+    library(required_packages_cut[i], character.only=TRUE)
   }
+}
+
+# Function to find missing packages
+findMissingPackages <- function(required_packages){
+  required_packages_cut <- cutTxt(x=required_packages, identifier="@", cut2="right") # Remove @ dev etc.
+  missing_packages <- required_packages[!(required_packages_cut %in% installed.packages()[ ,"Package"])]
+  missing_packages
 }
 
 # Function to install and/or load packages from CRAN 
-packagesCRAN <- function(requiredPackages, update=F){
-  missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
+packagesCRAN <- function(required_packages, update=FALSE){
+  missing_packages <- findMissingPackages(required_packages)
   
-  if(length(missingPackages) > 0 || update){
-    if(update){missingPackages <- requiredPackages} # Base (required)
-    install.packages(missingPackages)
+  if(length(missing_packages) > 0 || update){
+    if(update){missing_packages <- required_packages} # Base (required)
+    install.packages(missing_packages)
   }
   
-  loadLibraries(requiredPackages)
+  loadLibraries(required_packages)
 }
 
 # Function to install and/or load missing packages from Bioconductor 
-packagesBioconductor <- function(requiredPackages, update=F){
-  missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
+packagesBioconductor <- function(required_packages, update=FALSE){
+  missing_packages <- findMissingPackages(required_packages)
   
-  if(length(missingPackages) > 0 || update){
-    if(update){missingPackages <- requiredPackages} # Base (required)
+  if(length(missing_packages) > 0 || update){
+    if(update){missing_packages <- required_packages} # Base (required)
     dir.create("tmp")
     download.file(url="https://rawgit.com/greenore/initR/master/biocLite.R",
                   destfile="tmp/biocLite.R",
                   method=ifelse(Sys.info()["sysname"][[1]] == "Linux", "wget", "auto"))
     source("tmp/biocLite.R")
     unlink("tmp", recursive=TRUE)
-    biocLite(missingPackages)
+    biocLite(missing_packages)
   }
   
-  loadLibraries(requiredPackages)
+  loadLibraries(required_packages)
 }
 
 # Function to install and/or load missing packages from Github 
-packagesGithub <- function(requiredPackages, repoName, authToken=NULL, 
-                           proxyUrl=NULL, port=NULL,
-                           update=F){
+packagesGithub <- function(required_packages, repo_name, auth_token=NULL, 
+                           proxy_url=NULL, port=NULL,
+                           update=FALSE){
   packagesCRAN("devtools")
   
-  missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[ ,"Package"])]
+  missing_packages <- findMissingPackages(required_packages)
   
-  if(length(missingPackages) > 0 || update){
-    setProxy(proxyUrl=proxyUrl, port=port)
-    missingPackages <- paste0(repoName, '/', missingPackages)    # Base (missing)
+  if(length(missing_packages) > 0 || update){
+    setProxy(proxy_url=proxy_url, port=port)
+    full_repo_name <- paste0(repo_name, '/', missing_packages)    # Base (missing)
     
     if(update) {
-      missingPackages <- paste0(repoName, '/', requiredPackages) # Base (required)
+      full_repo_name <- paste0(repo_name, '/', required_packages) # Base (required)
     }
     
-    for(i in seq_along(missingPackages)){
-      install_github(repo=missingPackages[i], auth_token=authToken)
+    for(i in seq_along(missing_packages)){
+      install_github(repo=full_repo_name[i], auth_token=auth_token)
     }    
   }
   
-  loadLibraries(requiredPackages)
+  loadLibraries(required_packages)
 }
 
+## Proxy Functions
 # Function to ping a server (i.e., does the server exist)
-pingServer <- function(url, stderr=F, stdout=F, ...){
+pingServer <- function(url, stderr=FALSE, stdout=FALSE, ...){
   vec <- suppressWarnings(system2("ping", url, stderr=stderr, stdout=stdout, ...))
   if (vec == 0){TRUE} else {FALSE}
 }
 
 # Function to set a proxy
-setProxy <- function(proxyUrl, port){
+setProxy <- function(proxy_url, port){
   packagesCRAN("httr")
   
   port <- as.numeric(port)
   
-  if(pingServer(proxyUrl)){
+  if(pingServer(proxy_url)){
     usr <- readline("Bitte Benutzername eingeben: ")
     pwd <- readline("Bitte Passwort eingeben: ")
     cat("\14")
     
     reset_config()
-    set_config(use_proxy(url=proxyUrl, port=port, username=usr, password=pwd))
+    set_config(use_proxy(url=proxy_url, port=port, username=usr, password=pwd))
   }
 }
 
+## Additional helper functions
 # Cut txt to either the left or right of an identifier
 cutTxt <- function(x, identifier, regex="[[:alnum:]]{1, }", cut2="right"){
   if(cut2=="right"){
     x <- gsub(paste0(identifier, regex), "", x)
   }
-
+  
   if(cut2=="left"){
     x <- gsub(paste0(regex, identifier), "", x)
   }
